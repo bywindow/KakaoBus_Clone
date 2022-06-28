8e3423a39a3a0b64086df54a23d13114558edcd4
dionidip <paulus0617@gmail.com>
2022-06-29 01:19:16 +0900

    Add: WorkManager Configuration

--- app/src/main/java/com/example/kakaobus_clone/MainApplication.kt
+++ app/src/main/java/com/example/kakaobus_clone/MainApplication.kt
@@ -1,7 +1,11 @@
 package com.example.kakaobus_clone
 
 import android.app.Application
+import androidx.work.Configuration
 
-class MainApplication : Application() {
+
+class MainApplication : Application(), Configuration.Provider {
+    override fun getWorkManagerConfiguration(): Configuration =
+        Configuration.Builder().build()
 
 }
