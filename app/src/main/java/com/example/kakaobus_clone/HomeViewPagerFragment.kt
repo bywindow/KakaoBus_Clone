7a8b986bd7b08944b5a72b74f638dd19b5a9b667
dionidip <paulus0617@gmail.com>
2022-07-04 15:58:30 +0900

    fix: statusbar text color

--- app/src/main/java/com/example/kakaobus_clone/HomeViewPagerFragment.kt
+++ app/src/main/java/com/example/kakaobus_clone/HomeViewPagerFragment.kt
@@ -1,7 +1,23 @@
 package com.example.kakaobus_clone
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.kakaobus_clone.databinding.FragmentViewPagerBinding
 
 class HomeViewPagerFragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentViewPagerBinding.inflate(inflater, container, false)
+        val viewPager = binding.viewPager
+
+
+        return binding.root
+    }
 }
