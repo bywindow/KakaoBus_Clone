15e24ce302940adbfa059f921a072b799d675127
dionidip <paulus0617@gmail.com>
2022-07-07 23:53:23 +0900

    FEAT: Retrofit and OkHttp build

--- app/src/main/java/com/example/kakaobus_clone/api/BusPosService.kt
+++ app/src/main/java/com/example/kakaobus_clone/api/BusPosService.kt
@@ -1,6 +1,12 @@
 package com.example.kakaobus_clone.api
 
+import com.example.kakaobus_clone.data.BusPosByRouteStListResponse
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 /**
@@ -8,13 +14,30 @@ import retrofit2.http.GET
  */
 interface BusPosService {
 
-    //노선별 특정 정류소 접근 버스 위치정보 목록 조회
-    @GET()
-    suspend fun getBusPosByRouteStList(){
-
-    }
+    @GET("getBusPosByRouteSt")
+    suspend fun getBusPosByRouteStList(
+        @Query("ServiceKey") serviceKey: String,
+        @Query("busRouteId") busRouteId: String,
+        @Query("startOrd") startOrd: Int,
+        @Query("endOrd") endOrd: Int,
+    ): BusPosByRouteStListResponse
 
     companion object {
         private const val BASE_URL = "http://ws.bus.go.kr/api/rest/buspos/"
+
+        fun create(): BusPosService {
+            val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(BusPosService::class.java)
+        }
     }
 }
--- app/src/main/java/com/example/kakaobus_clone/data/BusPosByRouteStList.kt
+++ app/src/main/java/com/example/kakaobus_clone/data/BusPosByRouteStList.kt
@@ -6,15 +6,15 @@ data class BusPosByRouteStList(
     @field:SerializedName("sectOrd") val sectOrd: Int, //구간순번
     @field:SerializedName("sectDist") val sectDist: Int, //구간옵셋거리(km)
     @field:SerializedName("stopFlag") val stopFlag: Int, //정류소도착여부 (0:운행중, 1:도착)
-    @field:SerializedName("sectionId") val sectionId: Int, //구간 ID
+    @field:SerializedName("sectionId") val sectionId: String, //구간 ID
     @field:SerializedName("dataTm") val dataTm: String, //제공시간
     @field:SerializedName("tmx") val tmx: String, // 맵매칭 x 좌표 (WGS84)
     @field:SerializedName("tmy") val tmy: String, // 맵매칭 y 좌표 (WGS84)
-    @field:SerializedName("vehId") val vehId: Int, //버스 ID
+    @field:SerializedName("vehId") val vehId: String, //버스 ID
     @field:SerializedName("plainNo") val plainNo: String, //차량번호
     @field:SerializedName("busType") val busType: Int, //차량유형 (0:일반, 1:저상, 2:굴절)
-    @field:SerializedName("lastStnId") val lastStnId: Int, //최종정류장ID
+    @field:SerializedName("lastStnId") val lastStnId: String, //최종정류장ID
     @field:SerializedName("posX") val posX: String, //맵매칭 x 좌표 (GRS80)
     @field:SerializedName("posY") val posY: String, //맵매칭 y 좌표 (GRS80)
-    @field:SerializedName("routeId") val routeId: Int, //노선ID
+    @field:SerializedName("routeId") val routeId: String, //노선ID
 )
